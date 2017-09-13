1f1e97371a84c9826c6fcc116fb96fe11aeb9bcf
Nhu Pham <phamhuunhu21@gmail.com>
2017-09-13 14:30:07 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('helloword', 'MyController@HelloWord');
+
+Route::get('KhoaHoc', function() {
+    return "Xin chao cac ban";
+});
+
+Route::get('getinfo/{id}', 'MyController@getInfo');
+
+
+//Gửi và nhận Request
+Route::get('getForm', function() {
+	return view('postForm');
+});
+
+Route::post('postForm', ['as'=>'postForm', 'uses'=>'MyController@getName']);
